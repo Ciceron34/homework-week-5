d034a1348f132d7cf5d8651c41a12784f9e8a94a
Ciceron Chandler <ciceronchandler@gmail.com>
2017-04-04 19:56:12 -0400

    homework

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var body = document.getElementsByTagName("body")[0].children;
+console.log(body)
+
+
+// START TIMER
+// SHOW CONTAINER
+// BUTTON TO FINISH
+// GRAB BODY
+// GET BOOLEAN FROM CHECKED QUESTIONS
+// RETURN SCORE
+//
